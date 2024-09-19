5e57d51b830d3e49ac09bb39edd1da4a90e8059a
rani-neo <ishika.rashmi@gmail.com>
2024-09-19 18:03:31 +1000

    commited

--- 02.fundamental-part2/starter/assignment.js
+++ 02.fundamental-part2/starter/assignment.js
@@ -206,5 +206,112 @@ GOOD LUCK 😀
 // // console.log(`${john.fullName}'s BMI (${john.bmi} is heigher than ${mark.fullName}'s BMI (${mark.bmi) `)
 // }
 // 
-const calcTip 
 
+// Let's improve Steven's tip calculator even more, this time using loops!
+// 
+// 1. Create an array 'bills' containing all 10 test bill values
+// // 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// // 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips
+// // total values (bill + tip) for every bill value in the bills array. Use a for loop t
+// perform the 10 calculations!
+// 
+// TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// 
+// // HINT: Call calcTip in the loop and use the push method to add values to the tips an
+// totals arrays 😉
+// 
+// // 4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an 
+// // argument. This function calculates the average of all numbers in the given array. T
+// // is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  // // 4.1. First, you will need to add up all values in the array. To do the addition,
+// // by creating a variable 'sum' that starts at 0. Then loop over the array using a f
+// // loop. In each iteration, add the current value to the 'sum' variable. This way, b
+// end of the loop, you have all values added together
+  // // 4.2. To calculate the average, divide the sum you calculated before by the length
+// the array (because that's the number of elements)
+  // 4.3. Call the function with the 'totals' array
+// 
+//  const calcTip = function (bill) {
+  // return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//  }
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const total = [];
+
+// for (let i = 0; i < bills.length; i++){
+  // const tip = calcTip(bills[i]);
+  // tips.push(tip);
+  // total.push(tip + bills[i]);
+// }
+// console.log(bills, tips, total);
+
+// const calcAverage = function (arr) {
+  // let sum = 0;
+  // for (let i = 0; i < arr.length; i++) {
+    // sum += arr[i];
+  // }
+  // return sum / arr.length;
+// }
+// console.log(calcAverage([2, 3, 7]));
+// console.log(calcAverage(total));
+// console.log(calcAverage(tips));
+//Iteration: The for Loop﻿
+// There are elections in your country! in a small town, there are only 50 voters. Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'.
+
+// for (let i = 1; i <= 50; i++) {
+  // console.log(`Voter number ${i} is currently voting`);
+// }
+//Looping Arrays, Breaking and Continuing﻿
+// Let's bring back the populations array from a previous assignment.
+// 
+// // // Use a for loop to compute an array called percentages2 containing the percentages of the world population for the 4 population values. Use the function percentageWOrld1 that you created earlier.
+// 
+// // // Confirm that percentages2 contains exactly the same values as the percentages array that we created manually in the previous assignment, and reflect on how much better this solution is.
+// function percentageWorld1(population) {
+  //const worldPopulation = 7900; // World population in millions
+  //return (population / worldPopulation) * 100;
+// }
+// 
+// Array of populations (replace with your actual population values)
+// const populations = [1441, 83, 333, 126];
+// 
+///Array to store the percentages
+// const percentages2 = [];
+// for (let i = 0; i < populations.length; i++) {
+  // const percentage = percentageWorld1(populations[i]);
+//  percentages2.push(percentage);
+// }
+// console.log(percentages2);
+// 
+// Looping Backwards and Loops in Loops﻿
+// Store this array of arrays into a variable called listOfNeighbours:
+// 
+// [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+// // Log only the neighbouring countries to the console, one by one, not the entire arrays. Log a string like 'Neighbour: Canada' for each country.
+// 
+// // You will need a loop inside a loop for this. This is actually a bit tricky, so don't worry if it's too difficult for you! But you can still try to figure this out anyway 😉
+// 
+// const listOfNeighbours = [['Canada', 'mexixo'], ['spain'], ['Norway', 'Sweden', 'Russia']];
+// for (let i = 0; i < listOfNeighbours.length; i++) {
+  // for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    //  console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  // }
+// }
+// // Recreate the challenge from the lecture Looping Arrays, Breaking and Continuing, but this time using a while loop (call the array percentages3).
+// 
+// Reflect on what solution you like better for this task: the for loop or the while loop?
+
+// function percentageWorld1(population) {
+// const worldPopulation = 7900; // World population in millions
+// return (population / population) * 100;
+// }
+// const populations = [1441, 83, 333, 126];
+// const percentages3 = [];
+// let i = 0;
+// while (i < populations.length) {
+  //  percentages3.push(percentageWorld1(populations[i]));
+  // i++;
+// }
+// 
+// console.log(percentages3);
+// 
